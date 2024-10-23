216f809e32d13eb2ac1fcb582487ef87414fb639
Dan Adrian Mirea <damirea84@gmail.com>
2024-10-23 17:23:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# polar_hrm_test" 
---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# polar_hrm_test" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:danadrianmirea/polar_hrm_test.git
+git push -u origin main
